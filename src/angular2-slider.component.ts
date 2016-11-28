4ceee878bc9d1dec593910bdb827c8c4eb2b49ae
atecey <andrewtecey@hotmail.co.uk>
2016-11-28 16:15:21 +0000

    Setting up base project

---
+++ src/angular2-slider.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng2-slider',
+  templateUrl: './src/angular2-slider.component.html',
+})
+export class Ng2SliderComponent {
+
+  constructor() {
+  }
+}
