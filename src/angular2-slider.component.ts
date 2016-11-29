ff5379a87c7ae6fd3d981b23e04ae0e0663c5672
atecey <andrewtecey@hotmail.co.uk>
2016-11-29 09:51:13 +0000

    Updated component

--- src/angular2-slider.component.ts
+++ src/angular2-slider.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ng2-slider',
-  templateUrl: './src/angular2-slider.component.html',
+  templateUrl: './angular2-slider.component.html',
 })
 export class Ng2SliderComponent {
 
